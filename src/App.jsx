b85d45a62714d37a37d9b092f63eb050a0b4a0c6
ocruzmartz <00109321@uca.edu.sv>
2023-06-22 11:41:05 -0600

    Add responsive design in Get Started Component

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import { Navbar, GetStarted, About, Welcome, Footer } from "./components";
 
 function App() {
   return (
-    <div className="flex w-full flex-col">
-      <div className="bg-black flex w-full px-44 py-5 sticky top-0 z-50">
+    <div className="flex flex-col">
+      <div className="bg-black flex px-8 md:px-16 lg:px-44 py-5 sticky top-0 z-50">
         <Navbar />
       </div>
       <GetStarted />
--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -1,22 +1,21 @@
-
 import { volcano } from "../assets";
 
 const GetStarted = () => {
   return (
     <section className="relative h-[100vh]">
-     <div className="absolute inset-0 bg-gradient-to-b from-black"></div>
+      <div className="absolute inset-0 bg-gradient-to-b from-black"></div>
       <img src={volcano} alt="volcano" className="object-cover w-full h-full" />
-      <div className="absolute inset-0 flex flex-col px-32 py-36">
-        <h1 className="text-orange-500 text-[48px] font-bold text-left mb-3">
+      <div className="absolute inset-0 flex flex-col items-center justify-center mb-64 md:mb-0 md:items-start md:justify-start p-10 md:px-16 lg:px-36 py-16 md:py-36">
+        <h1 className="text-orange-500 text-[32px] md:text-[48px] font-bold text-center mb-3">
           Are you ready for action?
         </h1>
-        <p className="text-white text-xl mb-8">
+        <p className="text-white text-[14px] md:text-xl text-center mb-8">
           Now Undefeated is available for Android devices.
         </p>
-        <div>
+        <div className="flex justify-center">
           <button
             type="button"
-            className="px-5 py-3 rounded-full  font-bold text-black from-orange-500 bg-gradient-to-r to-yellow-500"
+            className="px-5 py-3 rounded-full font-bold text-black from-orange-500 bg-gradient-to-r to-yellow-500"
           >
             Download now
           </button>
